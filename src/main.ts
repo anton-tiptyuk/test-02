2dcdd83282017bb0996c3f4b0c6fce805e470b3a
Anton Tiptyuk <anton.tiptyuk@gmail.com>
2023-11-28 00:41:52 +0100

    adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { config } from './common/config';
 
@@ -6,6 +7,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  const options = new DocumentBuilder().setTitle('test-02').build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(config.webPort);
 }
 bootstrap();
